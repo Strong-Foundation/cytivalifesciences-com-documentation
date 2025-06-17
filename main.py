5b88cd2cb60440d6c3b227edc993e88837ea2dee
Strong-Foundation <179532304+Strong-Foundation@users.noreply.github.com>
2025-06-17 00:37:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -130,7 +130,7 @@ def main() -> None:
         system_path=file_name
     ):  # If the file doesn't exist after deletion
         apiRequestContent: str = getDataFromAPI(
-            page=0
+            page=1
         )  # Send POST request to get page of data
         append_write_to_file(
             system_path=file_name, content=apiRequestContent
